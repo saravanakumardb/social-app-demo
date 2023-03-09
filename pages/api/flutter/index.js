24e9226dfabbebe333981b39fa90aac92ba83f8b
Saravana Dhandapani <saravanakumardb@gmail.com>
2023-03-08 16:59:37 -0800

    Task 5: Create the insertOne endpoint
    https://github.com/saravanakumardb/social-app-demo/tree/5-lesson#task-5-create-the-insertone-endpoint

--- pages/api/flutter/index.js
+++ pages/api/flutter/index.js
@@ -27,6 +27,18 @@ export default async function handler(req, res) {
         const readDataJson = await readData.json();
         res.status(200).json(readDataJson.documents);
         break;
+      case "POST":
+        const flutter = req.body;
+        const insertData = await fetch(`${baseUrl}/insertOne`, {
+          ...fetchOptions,
+          body: JSON.stringify({
+            ...fetchBody,
+            document: flutter,
+          }),
+        });
+        const insertDataJson = await insertData.json();
+        res.status(200).json(insertDataJson);
+        break;
       default:
         res.status(405).end();
         break;
